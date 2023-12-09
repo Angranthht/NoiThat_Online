0c682929007c6b910c75fbe9a926d478b3f8c65f
Angranthht <2124802010726@student.tdmu.edu.vn>
2023-12-09 13:34:51 +0700

    first commit

---
+++ NoiThat/Areas/admin/Controllers/adminController.cs
@@ -0,0 +1,32 @@
+﻿using NoiThat.Controllers;
+using NoiThat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NoiThat.Areas.admin.Controllers
+{
+    public class adminController : Controller
+    {
+        public dbNoiThatOnlineDataContext data;
+
+        public adminController()
+        {
+            data = Connect.GetConnect();
+        }
+
+        public ActionResult Index()
+        {
+            if (Session["admin"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "loginAdmin");
+            }
+        }
+    }
+}
